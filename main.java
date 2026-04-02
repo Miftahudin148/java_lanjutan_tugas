1a816520a88e112f92c03d9b7ad576b7f7f7669c
Miftahudin148 <miftahhudin148@gmail.com>
2026-04-02 16:53:07 +0700

    Add files via upload

---
+++ main.java
@@ -0,0 +1,31 @@
+import model.Tiket;
+import model.TiketEkonomi;
+
+import payment.Pembayaran;
+import payment.TransferBank;
+import payment.EWallet;
+
+import service.TiketService;
+
+public class main {
+    public static void main(String[] args) {
+
+        // Service
+        TiketService service = new TiketService();
+
+        // Membuat tiket
+        Tiket tiket1 = new TiketEkonomi("Miftah", "A1", "Bandung");
+        service.tambahTiket(tiket1);
+
+        // Tampilkan tiket
+        service.tampilkanSemuaTiket();
+
+        System.out.println("=== PEMBAYARAN ===");
+
+        // Pembayaran
+        Pembayaran bayar1 = new TransferBank();
+        bayar1.prosesPembayaran(tiket1.getHarga());
+
+        System.out.println();
+    }
+}
